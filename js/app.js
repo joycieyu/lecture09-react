0a2b75f46819588c05ea77c4f479d80e3367a2b2
Joycie Yu <joycie@uw.edu>
2016-11-02 17:15:06 -0700

    complete lecture

--- js/app.js
+++ js/app.js
@@ -1,2 +1,28 @@
 'use strict';
 
+// <h1 id="hello" class="myClass">Hello Word</h1>
+// var message = React.createElement('h1', {id:'hello', className:'myClass'},
+//      'Hello World!');
+var header = <header className='well'><h1 id="hello" className="myClass">Hello JSX</h1></header>;
+
+//define a JS variable
+var imgUrl = 'http://goo.gl/n1tBtn';
+
+//include the URL in the JSX
+var pic = <img src={imgUrl} alt="A picture" />;
+
+
+//include an element variable inside JSX
+var main = <div>{header}<main>{pic}</main></div>;
+
+
+//declare a new class that IS A Component
+class HelloMessage extends React.Component {
+  render() {
+
+     return <h1>Hello Component!</h1>;
+  }
+}
+
+//show the content in the web page (inside #root)
+ReactDOM.render(<HelloMessage />, document.querySelector('#root'));
